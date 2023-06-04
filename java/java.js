b5ba57298f552ac62b4138ee776e628fab21c8ef
Magdy Mohammed Hanafy <mm312220@gmail.com>
2023-06-04 11:38:53 +0200

    Bug fixed

--- java/java.js
+++ java/java.js
@@ -1,147 +1,132 @@
 
 let taskData=[];
-let minI=0;
-function Display()
-{
-setTimeout(()=>{
-    var html="<table border='1|1' class='table'>";
-    html+="<thead>"
-    html+="<tr >"
-    html+="<td>No</td>"
-    html+="<td>Task Name</td>"
-    html+="<td>Periority</td>"
-    html+="<td>Action</td>"
-    html+="</tr>"
-    html+="</thead>"
-    
-    for(let i=0;i<taskData.length;i++)
+let addBtn=document.getElementById("addTask");
+let sortBtn=document.getElementById("sort");
+console.log(addBtn);
+
+let addTask=addBtn.addEventListener("click",function(){
+    let taskName=document.getElementById("taskName").value;
+    let periority=document.getElementById("Periority").value;
+    let  id=taskData.length+1;
+    let taskItems={taskName:taskName,periority:periority};
+    validate=validation(taskName,periority);
+    if(!validate)
     {
-        let Sno=i+1;
-        html+="<tbody>";
-        html+="<tr>";
-        html+="<td>"+ Sno +"</td>";
-        html+="<td>"+ taskData[i].task +"</td>";
-        html+="<td>"+ taskData[i].per +"</td>";
-        html+="<td>"+ `<button class="remove" onclick='deleteRow(${taskData[i].id})'>Delete</button>`
-        +`<button class="edit" onclick='editRow(${taskData[i].id})'>Edit</button>` +
-        "</td>";
-        html+="</tr>"
-        html+="<tbody>"; 
+     alert("Data not valid ,Try again");
+     return;
     }
+    addData(taskName,periority);
+    display(); 
+})
 
-    html+="<table>"
-    document.getElementById("dataTable").innerHTML=html;
-},200)
-
-}
-
-function addOnclick()
-{
-let task=document.getElementById("taskName").value;
-let per=document.getElementById("Periority").value;
-let check=parseInt(per);
-if(check<0)
-{
-    per=1;
-}
-if(check>3)
-{
-    per=3;
-}
-
-if(task && per)
-{
- let id=taskData.length+1;
- taskData.push({task:task,per:per,id:id});
- Display();
- takehightPeriority(per); 
-}
-}
-
-
-function clearFields()
-{
-document.getElementById("taskName").value="";
-document.getElementById("Periority").value="";
-}
-
-
-
-function deleteRow(rec)
-{
-     
- let filt=taskData.map((a,i)=>{
-        console.log(rec);
-        console.log(a.id);
-        console.log(i);
-   
-        if(rec==a.id)
-        {
-          
-            taskData.splice(i,1);
-            Display();
-        }
-        
-      
-    })
-   
-    
-}
-
-function editRow(rec)
-{
-   
-    let check=0;
-   let filt=taskData.map((a,i)=>{
-        let x=true;
-    if(rec==a.id)
+let validation=function(taskName,periority){
+    let taskNameCheck=false;
+    let periorityCheck=false;
+    if(!(taskName.length>10||taskName===""))
     {
-        if(x){
-           document.getElementById("save").addEventListener("click", function(){
-            let task=document.getElementById("taskName").value;
-            let per=document.getElementById("Periority").value;
-            let check=parseInt(per);
-            if(check<0)
-            {
-               per=1;
-            }
-               if(check>3)
-               {
-                 per=3;
-               }
-    
-               if(task && per)
-               {
-               taskData[i]={task:task,per:per,id:i+1};
-               console.log(taskData[i]);
-               } 
-            console.log("l");
-            Display();
-            x=false;
-
-          });
-        } 
+        taskNameCheck=true;
+    }
+    if(!(Number(periority)===NaN||Number(periority)<1))
+    {
+        periorityCheck=true;
+    }
+    if(taskNameCheck===true&&periorityCheck===true)  
+    {
+        return true;
     } 
-   })
-  }
-
-
-function takehightPeriority(per){
-    min=taskData[0].per;
-     minI=0;
-    for(let k=0;k<taskData.length;k++)
-    if(min>=taskData[k].per)
-    {
-     min=taskData[k].per;
-     minI=k;
-    }
-    return minI; 
-};
-
-function hightPeriority()
-{
-  console.log(taskData[minI]);
 }
+
+let addData=function(taskName,periority)
+{
+  let id=taskData.length+1;
+  let editMode=false;
+  taskData.push({taskName:taskName,periority:periority,id:id,editMode:editMode});
+}
+
+let display=function()
+{
+    let tableRows=  `<tr>
+                        <th>No</th>
+                        <th>Task Name</th>
+                        <th>Periority</th>
+                        <th>Delete</th>
+                        <th>Edit</th>
+                      </tr>`
+    for(let i=0 ;i<taskData.length;i++)
+    {
+      let No=i+1;
+      tableRows+=`<tr>
+        <td>${No}</td>
+        <td>${taskData[i].editMode ? `<input type="text" id="name" value="${taskData[i].taskName}"/>`: taskData[i].taskName}</td>
+        <td>${taskData[i].editMode ? `<input id="per" value="${taskData[i].periority}"/>`: taskData[i].periority}</td>
+        <td><button class="remove" onclick="deleteRow(${i})">Delete</button></td>
+        <td>${taskData[i].editMode? `<button class="save" onclick="saveEdit(${i})">Save</button>
+        <button class="cancel" onclick="cancelEdit(${i})">Cancel</button>`
+        : `<button class="edit" onclick="editRow(${i},${taskData[i].editMode})">Edit</button>`}
+        </td>
+        </tr>`                         
+    }
+    document.getElementById("dataTable").innerHTML=tableRows;
+    fSort();
+
+}
+
+let deleteRow=function(index)
+{
+    taskData.splice(index,1);
+    display();
+}
+
+
+let editRow=function(index,editMode)
+{
+    taskData[index].editMode=true;
+    display();
+}
+
+let saveEdit=function(index)
+{
+    taskData[index].taskName=document.getElementById("name").value;
+    taskData[index].periority=document.getElementById("per").value;
+    taskData[index].editMode=false;
+    display();
+}
+
+let cancelEdit=function(index)
+{
+    taskData[index].editMode=false;
+    display();
+}
+
+let fSort=function(){
+let sorting=sortBtn.addEventListener("click",function(){
+    taskData.sort(function (a, b) {
+        return a.periority - b.periority;
+    });
+   
+    display();    
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
